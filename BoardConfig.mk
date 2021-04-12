4851a093f09a47658d922ef001106adbb59a2ac9
J. Avila <elavila@google.com>
2021-04-12 18:22:36 +0000

    emerald: Explicitly set log_buf_len
    By setting the log_buf_len via cmdline, devices using this cmdline will
    be able to guarantee their preferred buffer size even if the underlying
    CONFIG_LOG_BUF_SHIFT changes.
    
    Bug: 185141673
    Change-Id: Iafe7561dceef65fc47cb0bb54a8f84a5e10c51a9
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>
    Signed-off-by: Nathan <nathannxx@proton.me>
    Signed-off-by: vannashcroft <vannashcroft@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += rcu_nocbs=all rcutree.enable_rcu_lazy=1
+BOARD_KERNEL_CMDLINE += log_buf_len=1024K
 BOARD_BOOTCONFIG := androidboot.serialconsole=0
 
 BOARD_KERNEL_PAGESIZE := 4096
