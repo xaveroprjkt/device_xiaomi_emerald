8b6c84ba4a7b41c3973ac76473bcc77472cd3199
guibing <guibing@google.com>
2023-10-25 17:26:08 +0000

    emerald: Set PELT HALFLIFE to 8ms before early init boot stage
    It's for early boot stage boost. After early init stage, change
    it back to the default 32ms.
    
    This is from the similar change we had in zuma: ag/23681609
    
    Bug: 300516320
    Test: check the device boot.
    Change-Id: I2286a94c31480766540e20cab619cc87363bdd1f
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>
    Signed-off-by: Nathan <nathannxx@proton.me>
    Signed-off-by: vannashcroft <vannashcroft@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_CMDLINE += rcu_nocbs=all rcutree.enable_rcu_lazy=1
 BOARD_KERNEL_CMDLINE += log_buf_len=1024K
+BOARD_KERNEL_CMDLINE += sysctl.kernel.sched_pelt_multiplier=4
 BOARD_BOOTCONFIG := androidboot.serialconsole=0
 
 BOARD_KERNEL_PAGESIZE := 4096
