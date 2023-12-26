17362d005fa375ef3513041a7ead3232baa193d7
bengris32 <bengris32@protonmail.ch>
2023-12-26 21:05:38 +0000

    emerald: Switch to common QTI vibrator service
    Change-Id: Ib58c8f5cd33ffbf0cf8c18ceb14c81cb21b1b2f9
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>

--- device.mk
+++ device.mk
@@ -372,8 +372,13 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
 # Vibrator
+$(call soong_config_set, vibrator, vibratortargets, vibratoraidlV2target)
+
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.mediatek
+    vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/emerald/emerald-vendor.mk)
