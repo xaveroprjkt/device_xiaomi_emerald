af96acb8ce889fec0827de9998bca883167647e1
Mashopy <eliasgheeraert@gmail.com>
2025-08-14 21:38:47 +0200

    emerald: Switch to AIDL bluetooth service
    Change-Id: I40550f0f2033e35ea609401f38f7646cb79abe14

--- device.mk
+++ device.mk
@@ -106,7 +106,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1-mtk-service \
+    android.hardware.bluetooth-service.mediatek \
     android.hardware.bluetooth.audio-impl:64
 
 # Boot control (A/B Updates)
