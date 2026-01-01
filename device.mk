9fbd27ae7635d56cda19fe93dfcf49103cf5e93f
vannxavero <vannxaveroo@proton.me>
2026-01-01 19:43:45 +0000

    Revert "emerald: Switch to AIDL bluetooth service"
    This reverts commit 5fa8915904fdd0954624e5fde4d63fd5fa4cfad7.

--- device.mk
+++ device.mk
@@ -117,7 +117,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth-service.mediatek \
+    android.hardware.bluetooth@1.1-mtk-service \
     android.hardware.bluetooth.audio-impl:64
 
 # Boot control (A/B Updates)
