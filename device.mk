5573a9641f2c0522bbb7eb46f4a893485d2c1420
claxten10 <claxten10@gmail.com>
2026-01-02 14:39:59 +0000

    emerald: Switch to AOSP Health AIDL
    * The previous MediaTek implementation had nothing MTK specific, and was essnetionally the same as
      the common AOSP implmenetation.
    
    * Import the chowns from the MediaTek health init.rc
    
    Change-Id: I6d45a1acbaebaf792b855bf201c1272032c0a375
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -166,8 +166,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.mediatek \
-    android.hardware.health-service.mediatek-recovery
+    android.hardware.health-service.example \
+    android.hardware.health-service.example_recovery
 
 # Light
 PRODUCT_PACKAGES += \
