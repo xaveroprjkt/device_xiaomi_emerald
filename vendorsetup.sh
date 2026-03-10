4d5a12635ff69e32cc95e966ae3f89bb719ef835
vannashcroft <vannashcroft@proton.me>
2026-03-10 22:15:03 +0100

    vendorsetup: Resolve Tracks Remotes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,9 +11,9 @@ rm -rf device/mediatek/sepolicy_vndr
 
 echo "Cloning Stuffs For Emeralds"
 
-git clone https://github.com/xiaomi-mt6789-devs/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
-git clone https://github.com/xiaomi-mt6789-devs/android_hardware_xiaomi.git -b lineage-23.1 hardware/xiaomi
-git clone https://github.com/xiaomi-mt6789-devs/android_device_mediatek_sepolicy_vndr -b lineage-23.2 device/mediatek/sepolicy_vndr
+git clone https://github.com/xiaomi-mt6789-dev/android_hardware_mediatek.git -b lineage-23.1 hardware/mediatek
+git clone https://github.com/xiaomi-mt6789-dev/android_hardware_xiaomi.git -b lineage-23.1 hardware/xiaomi
+git clone https://github.com/xiaomi-mt6789-dev/android_device_mediatek_sepolicy_vndr -b lineage-23.2 device/mediatek/sepolicy_vndr
 git clone https://github.com/xaveroprjkt/vendor_xiaomi_emerald.git -b lineage-23.2 vendor/xiaomi/emerald
 git clone https://github.com/xaveroprjkt/device_xiaomi_emerald-kernel.git -b lineage-22.2-mahiru device/xiaomi/emerald-kernel
 
