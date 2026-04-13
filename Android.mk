af104ad442c56fd2c139689d27acbe5f347e1100
Nathan <nathannxx@proton.me>
2026-04-13 01:36:07 +0000

    fixup: emerald: Upgrade mali blobs
    Change-Id: Ib59136d0d7fe9f59aa4f4b89dc64b2ca1d99ed5c
    Signed-off-by: Nathan <nathannxx@proton.me>
    Signed-off-by: vannashcroft <vannashcroft@proton.me>

--- Android.mk
+++ Android.mk
@@ -22,6 +22,7 @@ VENDOR_PLATFORM_LINKS := \
 	$(TARGET_OUT_VENDOR)/lib/libvcodecdrv_v3a.so \
 	$(TARGET_OUT_VENDOR)/lib/libvpudv3a_vcodec.so \
 	$(TARGET_OUT_VENDOR)/lib64/arm.graphics-V1-ndk_platform.so \
+	$(TARGET_OUT_VENDOR)/lib64/arm.graphics-V3-ndk.so \
 	$(TARGET_OUT_VENDOR)/lib64/egl/libGLES_mali.so \
 	$(TARGET_OUT_VENDOR)/lib64/gc16b3_aac_front_mipi_raw_IdxMgr.so \
 	$(TARGET_OUT_VENDOR)/lib64/gc16b3_aac_front_mipi_raw_n6r_IdxMgr.so \
