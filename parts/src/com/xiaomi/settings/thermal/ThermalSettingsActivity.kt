bbb6f9099eed9ed7fa13051dc2ea4e6681da8a08
AnierinB <anierin@evolution-x.org>
2026-04-30 01:21:17 +0700

    emerald: parts: Move to PreferenceFragmentCompat
    PreferenceFragment was deprecated in API level 28.
    
    https://developer.android.com/jetpack/androidx/releases/preference#1.1.0
    
    Change-Id: Ie8a4b04bd588afbbbc5882349ddbc05f988f80bf
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Nathan <nathannxx@proton.me>

--- parts/src/com/xiaomi/settings/thermal/ThermalSettingsActivity.kt
+++ parts/src/com/xiaomi/settings/thermal/ThermalSettingsActivity.kt
@@ -21,7 +21,7 @@ class ThermalSettingsActivity : CollapsingToolbarBaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        fragmentManager
+        supportFragmentManager
             .beginTransaction()
             .replace(
                 com.android.settingslib.collapsingtoolbar.R.id.content_frame,
