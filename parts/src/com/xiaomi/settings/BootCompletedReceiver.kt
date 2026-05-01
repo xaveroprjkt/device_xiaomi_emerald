4436b0dee4e3c613e99eea8025e98d0f07712a9c
Nathan <nathannxx@proton.me>
2026-05-01 14:22:34 +0700

    emerald: parts: Drop Charging Control
    Change-Id: Ifd07b05364b8f6940cec53d9791996ce96a69c96
    Signed-off-by: Nathan <nathannxx@proton.me>

--- parts/src/com/xiaomi/settings/BootCompletedReceiver.kt
+++ parts/src/com/xiaomi/settings/BootCompletedReceiver.kt
@@ -11,7 +11,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.UserHandle
 import android.util.Log
-import com.xiaomi.settings.battery.ChargingLimitService
 import com.xiaomi.settings.thermal.ThermalUtils
 
 /** Everything begins at boot. */
@@ -36,8 +35,5 @@ class BootCompletedReceiver : BroadcastReceiver() {
     private fun onLockedBootCompleted(context: Context) {
         // Thermal
         ThermalUtils.getInstance(context).startService()
-
-        // Battery
-        context.startServiceAsUser(Intent(context, ChargingLimitService::class.java), UserHandle.CURRENT)
     }
 }
