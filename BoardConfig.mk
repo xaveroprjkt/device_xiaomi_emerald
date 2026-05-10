kodeaqua/android_device_xiaomi_taiko

emerald: Add power HAL extension and use xiaomi-touch to handle dt2w
Change-Id: I5c65e0df255853eb2b3018a27ceb807c111bc4df
Signed-off-by: YagizErdemir06 <erdemiryagiz@proton.me>

BoardConfig.mk
@@ -150,6 +150,9 @@ TARGET_NO_BOOTLOADER := true
 # Platform
 TARGET_BOARD_PLATFORM := mt6789
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop

xaveroprjkt/device_xiaomi_emerald

Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

BoardConfig.mk
@@ -136,6 +136,9 @@ TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_SCREEN_DENSITY := 420
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop