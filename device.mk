966b13a4aa52b5d99a26bfe7578f78ddb001fd68
Ido <dev.xyzuniverse@gmail.com>
2026-07-14 17:04:24 +0200

    emerald: Import hotword blobs
    Change-Id: Id3062e317a524078105d279a10f5ea8e80daa77b
    Signed-off-by: Ido <dev.xyzuniverse@gmail.com>
    Signed-off-by: Nathan <nathannxx@proton.me>
    Signed-off-by: vannashcroft <vannashcroft@proton.me>

--- device.mk
+++ device.mk
@@ -183,6 +183,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.mediatek \
     android.hardware.health-service.mediatek-recovery
 
+# HotwordEnrollement
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Keymint
 PRODUCT_PACKAGES += \
     android.hardware.hardware_keystore_V3.xml
